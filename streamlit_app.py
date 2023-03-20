2c2e35b77e6d1423bb4352510dc3b45a88e9bc86
tyxsnow <127784511+tyxsnow@users.noreply.github.com>
2023-03-20 11:22:28 -0400

    Update streamlit_app.py
    added connexion

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,11 @@ import streamlit
 import snowflake.connector
 
 streamlit.title('Zenas Test')
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
+CURRENT_REGION()")
+
 
 
 
