363b107c08ba13c79d5552869ca0f25231ec681a
tyxsnow <127784511+tyxsnow@users.noreply.github.com>
2023-03-20 11:15:04 -0400

    Update streamlit_app.py
    added connexion

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,13 @@
 import streamlit
 import snowflake.connector
 
-streamlit.title('My parents new Heathly Diner')
+streamlit.title('Zenas Test')
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
+CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
